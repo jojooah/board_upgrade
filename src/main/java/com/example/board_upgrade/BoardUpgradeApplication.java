2ba3d215397e0c9a3e574d9e0b738a15c64ca7c4
jojooah <my_sg1@naver.com>
2022-02-20 10:18:53 +0900

    first commit

---
+++ src/main/java/com/example/board_upgrade/BoardUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardUpgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/board_upgrade/BoardUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardUpgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
